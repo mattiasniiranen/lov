fe8eb199d2f6fc9dc83252959b7e38116698a2b0
Mattias Niiranen <mattias@niiranen.net>
2016-03-17 16:05:07 +0100

    Add test suite

---
+++ lov/src/androidTest/kotlin/net/niiranen/lov/suite/InstrumentationTestSuite.kt
@@ -0,0 +1,9 @@
+package net.niiranen.lov.suite
+
+import net.niiranen.lov.LovInstrumentationTest
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+
+@RunWith(Suite::class)
+@Suite.SuiteClasses(LovInstrumentationTest::class)
+class InstrumentationTestSuite
